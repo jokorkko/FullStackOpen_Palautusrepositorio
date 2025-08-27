841d3ffaa39f97474c85600a6644fef678bd259c
jokorkko <94602273+jokorkko@users.noreply.github.com>
2025-08-27 14:28:10 +0300

    1.14
    Tehty tehtävät 1.12-1.14

---
+++ osa1/anekdootit/1.12/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  //let random_index = Math.floor(Math.random() * anecdotes.length)
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  console.log(votes)
+  console.log(selected)
+
+  const vote = (index) => {
+    const copy = [...votes]
+    copy[index] += 1
+    setVotes(copy)
+  }
+
+  const next_anecdote = () => {
+    const random_index = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random_index)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button onClick={() => vote(selected)} text='Vote'></Button>
+      <Button onClick={() => next_anecdote()} text='Next anecdote'></Button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+      <p>has {Math.max(...votes)} votes</p>
+    </div>
+  )
+}
+
+export default App
